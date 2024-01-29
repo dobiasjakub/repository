9c2a0d58e18de95c27d237c096d0272c391f1517
Jakub Dobiáš <dobiasj@outlook.cz>
2024-01-30 00:36:43 +0100

    deleting unused project

--- Java/my-app/src/main/resources/schema.sql
+++
@@ -1,8 +0,0 @@
-create table if not exists user (
-    id          identity        not null,
-    first_name  varchar(255)    not null,
-    last_name   varchar(255)    not null,
-    email       varchar(255)    not null,
-    password    varchar(255)    not null,
-);
-
